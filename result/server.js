1ddaf7cf311036f1fd249d7400091435926fb8a4
Yaksh Rajput <y7178435@gmail.com>
2025-11-29 20:09:36 +0530

    fixed vuln in manifests

--- result/server.js
+++ result/server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: process.env.DATABASE_URL
 });
 
 async.retry(
--- worker/Program.cs
+++ worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=supersecret123;");
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                            pgsql = OpenDbConnection("Server=db;Username=postgres;Password=supersecret123;");
                         }
                         else
                         { // Normal +1 vote requested
